034e9549608a52fa4d878c8665ddf2bf6d94234e
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-03-17 19:03:36 +0700

    hhhi

---
+++ src/rikkei/academy/AbsoluteNumberCaculator.java
@@ -0,0 +1,9 @@
+package rikkei.academy;
+
+public class AbsoluteNumberCaculator {
+    public static int findAbsolute(int number) {
+//        throw new UnsupportedOperationException();
+        if (number < 0) return -number;
+        return number;
+    }
+}
